871d0cc12224b09a2e3d4e8bf21f5ba5845fa668
venaaa <idaveee@gmail.com>
2020-03-21 14:40:33 +0300

    Added EDIT and REMOVE option. Added Auth for posts. Login..Logout

--- nails/urls.py
+++ nails/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views
 
 urlpatterns = [
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('account/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
     path('landing/', include('landing.urls')),
     path('admin/', admin.site.urls),
